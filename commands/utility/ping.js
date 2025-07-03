2be1146c5bf08a8c56b30b32da3d50917829a4ea
Blazzy <75049152+Blazzy12@users.noreply.github.com>
2025-07-03 17:21:50 -0400

    + Real ping command and hybrid

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	textEnabled: true,
 	category: 'utility',
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
+	async execute(interactionOrMessage) {
+		// Check if slash or text
+		const isSlashCommand = interactionOrMessage.isCommand?.() || interactionOrMessage.replied !== undefined;
+
+		const client = interactionOrMessage.client;
+
+		// Bot latency
+		const botLatency = client.ws.ping;
+		const responseMessage = `Pong! 🏓 Latency: ${botLatency}ms`;
+		return isSlashCommand
+			? await interactionOrMessage.reply({ content: responseMessage })
+			: await interactionOrMessage.reply(responseMessage);
 	},
 };
