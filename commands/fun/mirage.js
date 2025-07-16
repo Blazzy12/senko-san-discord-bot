58e46c32ea8ed17b96bbad7db5c87a378d4c484a
Blazzy <75049152+Blazzy12@users.noreply.github.com>
2025-07-16 15:43:12 -0400

    We do a little bit of trolling

--- commands/fun/mirage.js
+++ commands/fun/mirage.js
@@ -10,7 +10,7 @@ module.exports = {
 		// Check if slash or text
 		const isSlashCommand = interactionOrMessage.isCommand?.() || interactionOrMessage.replied !== undefined;
 
-		const content = 'https://tenor.com/view/anime-sleep-dizzy-gif-13520593';
+		const content = 'https://media.discordapp.net/attachments/817585952864075806/1395125879675293836/IMG_4493.gif?ex=68794fb9&is=6877fe39&hm=3c861cd7c68ea7340729baf3e3f386b1329e95867ddf26f0e2a49ea72cf4f761&=';
 		return isSlashCommand
 			? await interactionOrMessage.reply({ content })
 			: await interactionOrMessage.reply(content);
